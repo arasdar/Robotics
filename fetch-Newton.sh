6dfecb623b25d2074603760ba6f0728240fcdef2
arasdar <arasdar@ubuntu-mate-amd64-16.04>
2018-04-29 17:19:16 -0400

    NEW finroc files added.

---
+++ fetch-Newton.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o verbose #echo on
+
+mkdir -p temp
+cd temp
+ARCH=`arch`
+#wget http://newton-dynamics.googlecode.com/files/newton-dynamics-2.35.rar
+wget https://storage.googleapis.com/google-code-archive-downloads/v2/code.google.com/newton-dynamics/newton-dynamics-2.35.rar
+sudo apt-get install unrar
+unrar x newton-dynamics-2.35.rar newton-dynamics-2.35/coreLibrary_200
+mkdir -p newton-dynamics-2.35/packages/linux32
+mkdir -p newton-dynamics-2.35/packages/linux64
+cd newton-dynamics-2.35/coreLibrary_200/projets
+if [ $ARCH == 'x86_64' ]; then
+  cd linux64
+else
+  cd linux32
+fi
+make
+sudo cp libNewton.so /usr/local/lib
+sudo cp libNewton.a /usr/local/lib
+sudo cp ../../source/newton/Newton.h /usr/local/include
+
+
